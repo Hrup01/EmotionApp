f01395c444285f59a952fc112107f6e13190dc6c
Hrup01 <1448714441@qq.com>
2025-09-13 23:36:01 +0800

    创建了项目的基本架构 添加了项目的基本依赖

---
+++ src/test/java/com/groupb/EmotionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
