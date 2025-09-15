690c23de6003fbc8d20b8ffe26982496ac07c51b
zzzy23342 <zhao_200523@qq.com>
2025-09-15 21:59:50 +0800

    完成准备工作

---
+++ frontend/emotion-app/src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import request from '@/utils/request'
+export default {
+    name: 'LoginPage'
+}
+</script>
+
+<style lang="less">
+
+</style>
