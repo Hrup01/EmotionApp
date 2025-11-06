fa64f693d39a95317c32b3df057ed22b33144791
dsxy11npc <2900584594@qq.com>
2025-11-06 22:03:31 +0800

    添加junit依赖

--- src/main/java/com/groupb/mapper/UserMapper.java
+++ src/main/java/com/groupb/mapper/UserMapper.java
@@ -1,12 +1,13 @@
 package com.groupb.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.groupb.pojo.User;
 import org.apache.ibatis.annotations.*;
 
 import java.time.LocalDateTime;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User> {
     
     @Select("SELECT id, username, password, phone, avatar_url as avatarUrl, status, last_login_time as lastLoginTime, " +
             "create_time as createTime, update_time as updateTime FROM users WHERE username = #{username} LIMIT 1")
--- src/main/java/com/groupb/pojo/User.java
+++ src/main/java/com/groupb/pojo/User.java
@@ -1,5 +1,6 @@
 package com.groupb.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("users")
 public class User {
     private long id;//id(主键)
     private String phone;//手机号
